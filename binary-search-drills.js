e500b4af70b8cc760c6184aea37fbaa03ecf57f1
seankrummel <sean.krummel@gmail.com>
2019-01-23 11:44:31 -0600

    added remove method to BinaryTree

--- binary-search-drills.js
+++ binary-search-drills.js
@@ -15,16 +15,64 @@ class BinaryTree {
       this.value = value;
     }
 
-    if (key < this.key) {
+    if (this.key > key) {
       if (this.left === null) {
         this.left = new BinaryTree(key, value, this);
       } else {
         this.left.insert(key, value);
       }
-    } else if (this.right === null){
-      this.right = new BinaryTree(key, value, this);
     } else {
-      this.right.insert(key, value);
+      if (this.right === null){
+        this.right = new BinaryTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  _findLargest() {
+    if (this.right) return this.right._findLargest();
+    else return this;
+  }
+  remove(key) {
+    if (this.key === key) {
+      const parent = this.parent;
+      if (!this.left && !this.right) {
+        // remove if no children
+        if (parent.left.key === key) parent.left = null;
+        else parent.right = null;
+      } else if (this.left && this.right) {
+        // remove if two children
+        const largest = this.left._findLargest();
+        if (largest.parent.left === largest) largest.parent.left = null;
+        else largest.parent.right = null;
+        largest.parent = parent;
+        largest.right = this.right;
+        largest.left = this.left;
+      } else {
+        // remove if one child
+        if (this.left) {
+          this.left.parent = parent;
+          this.left = null;
+        } else {
+          this.right.parent = parent;
+          this.right = null;
+        }
+      }
+    } else if (this.key > key) {
+      if (this.left === null) {
+        // key is not in the tree
+        throw new Error('Key Error');
+      } else {
+        this.left.remove(key);
+      }
+    } else {
+      if (this.right === null) {
+        // key is not in the tree
+        throw new Error('Key Error');
+      } else {
+        this.right.remove(key);
+      }
     }
   }
 
